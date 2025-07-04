f5c999c60dba782372c17452e77b2eb6dace07be
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2025-07-04 12:07:24 +0530

    Update Message.jsx

--- src/Pages/Message.jsx
+++ src/Pages/Message.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { FaCopy } from "react-icons/fa";
 const Message = ({ chatdata ,scrollRef }) => {
+    const [Iscopied,setIsCopied]=useState(false)
     console.log(chatdata, "chatdata");
+
+    useEffect(()=>{
+if(Iscopied==true){
+    setTimeout(() => {
+        setIsCopied(false)
+    }, 500);
+}
+    },[Iscopied])
     return (
         chatdata && chatdata.map(function (caaavalue, index) {
             return (
@@ -14,6 +23,18 @@ const Message = ({ chatdata ,scrollRef }) => {
                
                     <div className="Received-Parent">
                         {caaavalue.AI_RESPONSE}
+                    </div>
+                    <div className="Copy-parent">
+                        {Iscopied==false?
+                        (
+                             <FaCopy className="Copy_Button" onClick={()=> {navigator.clipboard.writeText(caaavalue.AI_RESPONSE),setIsCopied(true)}}/>
+                        ):(
+                         <p>Copied....</p>
+ 
+                        )
+                        }
+                   
+                  
                     </div>
                     <div ref={scrollRef}></div>
                 </div>
