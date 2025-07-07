1cf57c08147ecf9656eefdbf8fdc20d1df377f86
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2025-07-07 11:26:42 +0530

    Update Message.jsx

--- src/Pages/Message.jsx
+++ src/Pages/Message.jsx
@@ -1,45 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { FaCopy } from "react-icons/fa";
-const Message = ({ chatdata ,scrollRef }) => {
-    const [Iscopied,setIsCopied]=useState(false)
-    console.log(chatdata, "chatdata");
 
-    useEffect(()=>{
-if(Iscopied==true){
-    setTimeout(() => {
-        setIsCopied(false)
-    }, 500);
-}
-    },[Iscopied])
+const Message = ({ chatdata, scrollRef }) => {
+    const [copiedIndex, setCopiedIndex] = useState(null);
+
+    useEffect(() => {
+        if (copiedIndex !== null) {
+            const timer = setTimeout(() => setCopiedIndex(null), 1000);
+            return () => clearTimeout(timer);
+        }
+    }, [copiedIndex]);
+
     return (
-        chatdata && chatdata.map(function (caaavalue, index) {
-            return (
-                <div className="Message-Parent" key={index}>
-              
-                    <div className="Sending-parent">
-                        {caaavalue.USER_RESPONSE}
-                    </div>
-
-               
-                    <div className="Received-Parent">
-                        {caaavalue.AI_RESPONSE}
-                    </div>
-                    <div className="Copy-parent">
-                        {Iscopied==false?
-                        (
-                             <FaCopy className="Copy_Button" onClick={()=> {navigator.clipboard.writeText(caaavalue.AI_RESPONSE),setIsCopied(true)}}/>
-                        ):(
-                         <p>Copied....</p>
- 
-                        )
-                        }
-                   
-                  
-                    </div>
-                    <div ref={scrollRef}></div>
+        chatdata &&
+        chatdata.map((caaavalue, index) => (
+            <div className="Message-Parent" key={index}>
+                <div className="Sending-parent">
+                    {caaavalue.USER_RESPONSE}
                 </div>
-            );
-        })
+
+                <div className="Received-Parent">
+                    {caaavalue.AI_RESPONSE}
+                </div>
+
+                <div className="Copy-parent">
+                    {copiedIndex === index ? (
+                        <p className="P-copy">Copied...</p>
+                    ) : (
+                        <FaCopy
+                            className="Copy_Button"
+                            onClick={() => {
+                                navigator.clipboard.writeText(caaavalue.AI_RESPONSE);
+                                setCopiedIndex(index);
+                            }}
+                        />
+                    )}
+                </div>
+
+                <div ref={scrollRef}></div>
+            </div>
+        ))
     );
 };
 
